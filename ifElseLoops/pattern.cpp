7a913f2892a40290d7e180a41ca680aaaf738d8e
Gaurav Trivedi <trivedig319@gmail.com>
2023-04-22 22:54:34 +0530

    pattern

--- ifElseLoops/pattern.cpp
+++ ifElseLoops/pattern.cpp
@@ -1,86 +1,72 @@
 #include<iostream>
 using namespace std;
 
-// int main(){
-//     // five star
-//     int total_rows;
-//     cin>> total_rows;
+int main(){
 
-//     for(int row=1; row<= total_rows; row++){
-//         for(int col=1; col<= (total_rows-row)){
+    int total_row;
+    cin>> total_row;
 
+    for(int row=1; row<=total_row; row++){
+        // space
+        for(int col=1; col<=total_row-row; col++){
+            cout<<" ";
+        }
+
+        // star
+        for(int col=1; col<=row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+// //butterfly
+// int main()
+// {
+
+//     int n;
+//     cin>> n;
+
+//     // first half
+//     for(int row=1; row<=n; row++){
+//         // star
+//         for(int col=1; col<=row; col++){
+//             cout<<"*";
 //         }
-//     }
-// }
-
-
-
-// int main(){
-
-//     int total_row;
-//     cin>> total_row;
-
-//     for(int row=1; row<=total_row; row++){
+        
 //         // space
-//         for(int col=1; col<=total_row-row; col++){
+//         for(int col=1; col<=((2*n)-(2*row)); col++){
 //             cout<<" ";
 //         }
 
 //         // star
 //         for(int col=1; col<=row; col++){
-//             cout<<"* ";
+//             cout<<"*";
 //         }
 //         cout<<endl;
 //     }
+
+//     // 2nd half
+//     for(int row=n-1; row>=1; row--){
+//         // star
+//         for(int col=1; col<=row; col++){
+//             cout<<"*";
+//         }
+        
+//         // space
+//         for(int col=1; col<=((2*n)-(2*row)); col++){
+//             cout<<" ";
+//         }
+
+//         // star
+//         for(int col=1; col<=row; col++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
 // }
-
-
-// butterfly
-int main()
-{
-
-    int n;
-    cin>> n;
-
-    // first half
-    for(int row=1; row<=n; row++){
-        // star
-        for(int col=1; col<=row; col++){
-            cout<<"*";
-        }
-        
-        // space
-        for(int col=1; col<=((2*n)-(2*row)); col++){
-            cout<<" ";
-        }
-
-        // star
-        for(int col=1; col<=row; col++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-
-    // 2nd half
-    for(int row=n-1; row>=1; row--){
-        // star
-        for(int col=1; col<=row; col++){
-            cout<<"*";
-        }
-        
-        // space
-        for(int col=1; col<=((2*n)-(2*row)); col++){
-            cout<<" ";
-        }
-
-        // star
-        for(int col=1; col<=row; col++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-
-}
 
 
 
